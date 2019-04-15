abdab66c1cee8b7d47123b42cdac94cef16d908a
brian02oriel <brian.nieto1@utp.ac.pa>
2019-04-15 12:53:41 -0500

    Módulo para manejo de usuarios listo

---
+++ src/models/user.js
@@ -0,0 +1,85 @@
+const connection = require('./connection');
+
+let userModel = {};
+
+//Select all users for evaluate if the account exists
+userModel.getUsers = (callback) => {
+    if(connection){
+        connection.query('SELECT * FROM users ORDER BY id', 
+        (err, rows) => {
+            if(err){
+                console.log(err);
+            } else {
+                callback(null, rows);
+                }
+            }
+        )
+    }
+};
+
+//Insert users to the db to sign up
+userModel.insertUser = (userData, callback) => {
+    if(connection){
+        connection.query('INSERT INTO users SET ?', userData,
+        (err, rows) =>{
+            if(err){
+                console.log(err);
+            } else {
+                callback(null, {
+                    'insertId': rows.insertId
+                })
+            }
+            
+        })
+    }
+}
+
+//Update user information
+userModel.updateUser = (userData, callback) => {
+    if(connection){
+        const query = "UPDATE users SET email = " +
+         connection.escape(userData.email) +
+         " , password = " + connection.escape(userData.password) + 
+         " WHERE id = " + connection.escape(userData.id) + " ;"
+        
+         connection.query(query, (err, rows) =>{
+            if(err){
+                console.log(err);
+            } else {
+                callback(null, {
+                    "msg": "success"
+                });
+            }
+        });
+    }
+}
+
+//Delete an existing user
+userModel.deleteUser = (userId, callback) => {
+    if(connection){
+        let query = "SELECT * FROM users WHERE id = " + connection.escape(userId) + " ;"
+        connection.query(query, (err, row) =>{
+            if(row.length !== 0){
+                let query = "DELETE FROM users WHERE id = " + connection.escape(userId) + " ;"
+                connection.query(query, (err, row) =>{
+                    if(err){
+                        console.log(err);
+                    } else {
+                        callback(null, {
+                            "msg": "success"
+                        });
+                    }
+                }) 
+            } else{
+                callback(null, {
+                    "msg": "This user dont exist"
+                })
+            }
+       
+        
+         
+        });
+    }
+}
+
+module.exports = userModel;
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,92 @@
+const User = require('../models/user');
+
+
+module.exports = function (app){
+    app.get('/', (req, res)=>{
+        res.json([]);
+    });
+
+    //Get existing users
+    app.get('/users', (req, res) =>{
+        console.log(User);
+        User.getUsers((err, data) => {
+            res.json(data);
+        });
+    });    
+
+    //Insert new users
+    app.post('/users', (req, res) =>{
+        const userData = {
+            id: null,
+            rol: req.body.rol,
+            email: req.body.email,
+            password: req.body.password,
+            company: req.body.company,
+            created_at: null,
+        };
+        
+        User.insertUser(userData, (err, data) =>{
+            if(data && data.insertId){
+                res.json({
+                    success: true,
+                    msg: 'User inserted',
+                    data: data   
+                })
+            } else {
+                res.status(500).json({
+                    success: false,
+                    msg: 'Error: Cannot insert user'
+
+                })
+            }
+        })
+    })
+
+    //Update user information
+    app.put('/users', (req, res) =>{
+        const userData = {
+            id: req.body.id,
+            rol: req.body.rol,
+            email: req.body.email,
+            password: req.body.password,
+            company: req.body.company,
+            created_at: null,
+        };
+
+        User.updateUser(userData, (err, data) =>{
+            if(data && data.msg){
+                res.json({
+                    success: true,
+                    msg: 'User updated',
+                    data: data   
+                })
+            } else {
+                res.status(500).json({
+                    success: false,
+                    msg: 'Error: Cannot update user'
+
+                })
+            }
+        })
+    })
+
+
+    app.delete('/users', (req, res) =>{
+        User.deleteUser(req.body.id, (err, data) =>{
+            
+            if(data && data.msg === "success"){
+                res.json({
+                    success: true,
+                    msg: 'User deleted',
+                    data: data   
+                })
+            } else {
+                res.status(500).json({
+                    success: false,
+                    msg: 'Error: This user dont exist'
+
+                })
+            }
+        })
+    })
+}
